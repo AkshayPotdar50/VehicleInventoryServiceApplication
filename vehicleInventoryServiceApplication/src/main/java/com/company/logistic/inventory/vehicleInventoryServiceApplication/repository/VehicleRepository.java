bf2491143792cb9c24daccec1282b368653c06bc
Akshay Potdar50 <potdarakshay50@gmail.com>
2026-02-11 17:33:05 +0530

    second commit

--- vehicleInventoryServiceApplication/src/main/java/com/company/logistic/inventory/vehicleInventoryServiceApplication/repository/VehicleRepository.java
+++ vehicleInventoryServiceApplication/src/main/java/com/company/logistic/inventory/vehicleInventoryServiceApplication/repository/VehicleRepository.java
@@ -1,7 +1,20 @@
 package com.company.logistic.inventory.vehicleInventoryServiceApplication.repository;
 
+import com.company.logistic.inventory.vehicleInventoryServiceApplication.enums.VehicleStatus;
 import com.company.logistic.inventory.vehicleInventoryServiceApplication.model.Vehicle;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.util.Optional;
+
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    Optional<Vehicle>findByRegistrationNumber(String registrationNumber);
+
+    boolean existsByRegistrationNumber(String registrationNumber);
+
+    Page<Vehicle> findByStatus(VehicleStatus status, Pageable pageable);
 }
